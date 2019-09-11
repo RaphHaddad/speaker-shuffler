402b2eebdbc3f3a5e485e01e3ab2957fb8a3ffc3
me@raph.ws <raph>
2019-09-11 22:15:45 +1000

    Still compiles

--- src/Client/Client.fs
+++ src/Client/Client.fs
@@ -13,66 +13,19 @@ open Shared
 
 type Speaker = { Name: string }
 
-// The model holds data that you want to keep track of while the application is running
-// in this case, we are keeping track of a counter
-// we mark it as optional, because initially it will not be available from the client
-// the initial value will be requested from server
 type Model = { Counter: Counter option }
 
-// The Msg type defines what events/actions can occur while the application is running
-// the state of the application changes *only* in reaction to these events
 type Msg =
     | AddSpeaker
     | ShuffleSpeakers
 
-let initialCounter () = Fetch.fetchAs<Counter> "/api/init"
-
-// defines the initial state and initial command (= side-effect) of the application
-let init () : seq<Speaker> * Cmd<Msg> =
+let init () =
     (Seq.empty<Speaker>), Cmd.ofMsg AddSpeaker
 
-// The update function computes the next state of the application based on the current state and the incoming events/messages
-// It can also run side-effects (encoded as commands) like calling the server via Http.
-// these commands in turn, can dispatch messages to which the update function will react.
-let update (msg : Msg) (currentModel : seq<Speaker>) : seq<Speaker> * Cmd<Msg> =
+let update msg currentModel =
     currentModel, Cmd.ofMsg msg
 
-
-let safeComponents =
-    let components =
-        span [ ]
-           [ a [ Href "https://github.com/SAFE-Stack/SAFE-template" ]
-               [ str "SAFE  "
-                 str Version.template ]
-             str ", "
-             a [ Href "https://saturnframework.github.io" ] [ str "Saturn" ]
-             str ", "
-             a [ Href "http://fable.io" ] [ str "Fable" ]
-             str ", "
-             a [ Href "https://elmish.github.io" ] [ str "Elmish" ]
-             str ", "
-             a [ Href "https://fulma.github.io/Fulma" ] [ str "Fulma" ]
-
-           ]
-
-    span [ ]
-        [ str "Version "
-          strong [ ] [ str Version.app ]
-          str " powered by: "
-          components ]
-
-let show = function
-    | { Counter = Some counter } -> string counter.Value
-    | { Counter = None   } -> "Loading..."
-
-let button txt onClick =
-    Button.button
-        [ Button.IsFullWidth
-          Button.Color IsPrimary
-          Button.OnClick onClick ]
-        [ str txt ]
-
-let view (speaker : seq<Speaker>) (dispatch : Msg -> unit) =
+let view model dispatch =
     div []
         [
             form [] [ h2 [] [str "Enter Speakers Seperated by a new line"]]
