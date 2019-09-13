775a6f094cbb24caf8b8d0970f1aaea8e08890f6
me@raph.ws <raph>
2019-09-14 09:57:57 +1000

    Add view

--- src/Client/Client.fs
+++ src/Client/Client.fs
@@ -29,17 +29,17 @@ let update msg currentModel =
 let view model dispatch =
     div [] [
         h1 [] [ str "SpeekUp speaker shuffler" ]
-        // h2 [] [str "Enter Speakers Seperated by a new line"]
-        // div [] [
-            // input [
-            //     OnChange (fun ev -> ev.Value
-            //                         |> AddSpeaker
-            //                         |> dispatch)
-            // ]
-            // Button.span [
-            //     Button.OnClick (fun _ -> dispatch ShuffleSpeakers)
-            //  ] [str "Shuffle Speakers"]
-        // ]
+        h2 [] [str "Enter Speakers Seperated by a new line"]
+        div [] [
+            input [
+                OnChange (fun ev -> ev.Value
+                                    |> AddSpeaker
+                                    |> dispatch)
+            ]
+            Button.span [
+                Button.OnClick (fun _ -> dispatch ShuffleSpeakers)
+             ] [str "Shuffle Speakers"]
+        ]
     ]
 
 #if DEBUG
