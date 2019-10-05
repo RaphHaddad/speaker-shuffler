4556e39593a5592e36c464a7883071f2c9c04de0
me@raph.ws <raph>
2019-10-05 17:19:01 +1000

    Split tests

--- src/Tests/AcceptanceTests.fs
+++ src/Tests/AcceptanceTests.fs
@@ -1,52 +1,11 @@
-module ShuffleTests
+module AcceptanceTests
 
 open Expecto
 open Types
 open Shuffle;
 
 [<Tests>]
-let ShuffleRules =
-  let testFx = 1000000000
-
-  testList "ShuffleRules" [
-      testCase "speakerIntroers shouldn't introduce themselves" <| fun _ ->
-            let test =
-              let people = [{ Name = "Raph"; Order = 0 }
-                            { Name = "Dave"; Order = 0 }
-                            { Name = "Jane"; Order = 0 }
-                            { Name = "Alex"; Order = 0 }]
-
-              let speakersIntroers = shuffle people
-
-              speakersIntroers.Speakers
-              |> Seq.iter2 (fun speaker introer ->
-                                (Expect.notEqual speaker.Name introer.Name "a speaker shouldn't introduce themselves")) speakersIntroers.Introducers
-
-            [testFx]
-            |> Seq.iter (fun _ -> test)
-
-      testCase "speakerIntroers shouldn't introduce after speaking" <| fun _ ->
-            let test =
-                let people = [{ Name = "Raph"; Order = 0 }
-                              { Name = "Dave"; Order = 0 }
-                              { Name = "Jane"; Order = 0 }]
-
-                let speakersIntroers = shuffle people
-
-                speakersIntroers.Speakers
-                |> Seq.iteri2 (fun i _ speaker ->
-                                  let introer = speakersIntroers.Introducers |> Seq.tryItem(i + 1)
-                                  match introer with
-                                  | Some (introer) ->
-                                                Expect.notEqual speaker.Name introer.Name "Shouldn't introduce after speaking"
-                                  | None -> ()) speakersIntroers.Introducers
-
-            [testFx]
-            |> Seq.iter (fun _ -> test)
-  ]
-
-[<Tests>]
-let AcceptanceTests =
+let Tests =
   let testFx = 10000000
 
   testList "AcceptanceTests" [
